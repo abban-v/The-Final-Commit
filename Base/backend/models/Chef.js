1b386b41ca945f7c0da4701e6862ec7f7aa65916
Abban C Varughese <abban.varughese@gmail.com>
2025-10-04 16:28:28 +0000

    cleaning up

--- Base/backend/models/Chef.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChefSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Chef', ChefSchema);
